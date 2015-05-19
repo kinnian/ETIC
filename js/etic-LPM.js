8d0a4875b402e2bb2ab3af0fa20aa30d398f77ab
kinnian <julia.c.tth@gmail.com>
2015-05-19 17:08:54 +0200

    Zoom In/ Zoom Out
    Added the zoom in / zomm out function.

--- js/etic-LPM.js
+++ js/etic-LPM.js
@@ -11,3 +11,21 @@ function hide(id)
     document.getElementById(id).style.visibility="hidden";
     return true;
 }
+
+function zoomIn(id)
+{
+	document.getElementById(id).style.zoom = "1.5";
+	document.getElementById(id).style.MozTransform = 'scale(1.5)';
+	document.getElementById(id).style.WebkitTransform = 'scale(1.5)';
+	document.getElementById(id).style.margin = "10%";
+	return true;
+}
+
+function zoomOut(id)
+{
+	document.getElementById(id).style.zoom = "1";
+	document.getElementById(id).style.MozTransform = 'scale(1)';
+	document.getElementById(id).style.WebkitTransform = 'scale(1)';
+	document.getElementById(id).style.margin = "3%";
+	return true;
+}
