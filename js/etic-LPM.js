15666abab6c65c30654e9db5966b50c0fbf26039
kinnian <julia.c.tth@gmail.com>
2015-05-19 14:54:01 +0200

    First commit
    Working show/hide text.

---
+++ js/etic-LPM.js
@@ -0,0 +1,13 @@
+
+
+function show(id)
+{
+    document.getElementById(id).style.visibility="visible";
+    return true;
+}
+
+function hide(id)
+{
+    document.getElementById(id).style.visibility="hidden";
+    return true;
+}
